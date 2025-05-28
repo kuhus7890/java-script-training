24febfa3d06e346acaf8759e60beca356fa15aef
kuhus7890 <kuhus7890@gmail.com>
2025-05-28 11:40:52 +0530

    Add files via upload

---
+++ day7.js
@@ -0,0 +1,4 @@
+const ob1={name:"ABES EC"};
+const ob2={address:"ghaziabad"};
+console.log(ob1)
+console.log(ob2)
---
+++ day7_array.js
@@ -0,0 +1,19 @@
+/*const student=["run","mohan","prince"];
+console.log(student[0]);
+console.log(student[1]);
+console.log(student[2]);
+*/
+const arr = new Array(10, 20, 30, 40);
+const arr1 = [...arr];            
+const arr2 = new Array(10);       
+const arr3 = [10];  
+console.log(arr);
+console.log(arr1);
+console.log(arr2)
+console.log(arr3);
+console.log(arr.toString());
+console.log(arr.length);
+console.log(arr.sort());
+let string = "GeeksforGeeks";
+console.log(string.search());
+
